feae03e5043f0cd1f9a98fb2ed0858c79798bd7e
Okhan-coder <O_khan4@u.pacific.edu>
2022-03-25 21:14:06 -0700

    Posting Works
    Posting is working

--- Parsagram/CameraViewController.swift
+++ Parsagram/CameraViewController.swift
@@ -6,8 +6,9 @@
 //
 
 import UIKit
-
-class CameraViewController: UIViewController {
+import AlamofireImage
+import Parse
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var commentField: UITextField!
@@ -18,8 +19,45 @@ class CameraViewController: UIViewController {
     }
     
     @IBAction func onSubmitButton(_ sender: Any) {
+        let post = PFObject(className: "Posts")
+        post["caption"] = commentField.text
+        let imageData = imageView.image!.pngData()
+        post["author"] = PFUser.current()!
+        let file = PFFileObject(data: imageData!)
+        
+        post["image"] = file
+        
+        post.saveInBackground { (success, error )in
+            if success {
+                self.dismiss(animated: true, completion: nil)
+            }
+            else{
+                print("Error!")
+            }
+        }
+        
     }
     @IBAction func onCameraButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            picker.sourceType = .camera
+        
+        }
+        else{
+            picker.sourceType = .photoLibrary
+        }
+        present(picker, animated:true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image=info[.editedImage] as! UIImage
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage=image.af_imageScaled(to: size)
+        imageView.image=scaledImage
+        dismiss(animated: true, completion: nil )
+        
     }
     
     /*
